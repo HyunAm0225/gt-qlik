99af5d4d967b4baeab2a21173e28e81dd884d243
HyunAm0225 <tlfgjawlq@naver.com>
2020-04-03 15:22:12 +0900

    login 50%

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import CustomUserCreationform
 from django.contrib.auth import login as auth_login
 from django.shortcuts import redirect
+from django.contrib import auth
 
 # Create your views here.
 def signup(request):
@@ -18,4 +19,20 @@ def signup(request):
 })
 
 def login(request):
-    return render(request,'login.html')
+    if request.method =="POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username = username, password = password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html',{'error': 'username or password is incorrect.'})
+    else: 
+        return render(request,'login.html')
+
+def logout(request):
+    if request.method =="POST":
+        auth.logout(request)
+        return redirect('home')
+    return render(request, 'signup.html')
