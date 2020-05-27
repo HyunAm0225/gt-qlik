bad3b581957a2bae860798c981bc79d0479305ca
HyunAm0225 <tlfgjawlq@naver.com>
2020-05-27 16:11:51 +0900

    django-import-export추가

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-
+from import_export.admin import ExportActionModelAdmin, ImportExportMixin, ImportMixin
 from .models import User
 # Register your models here.
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ('name','dept_name','rank','email')
     search_fields = ['name']
 admin.site.register(User,UserAdmin)
