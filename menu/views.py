439111fa9e63d34942a80047e9f06319b6a1179c
HyunAm0225 <tlfgjawlq@naver.com>
2020-06-04 15:01:52 +0900

    페이지네이션 추가

--- menu/views.py
+++ menu/views.py
@@ -13,7 +13,7 @@ from .models import Menu
 @method_decorator(login_message_required, name='dispatch')
 class MenuListView(ListView):
     model = Menu
-    paginated_by = 10
+    paginate_by = 10
     template_name = 'menu_list.html'
     context_object_name = 'menu_list'
 
@@ -21,23 +21,23 @@ class MenuListView(ListView):
         menu_list = Menu.objects.order_by('menu_rank')
         return menu_list
 
-def get_context_data(self,**kwargs):
-    context = super().get_context_data(**kwargs)
-    paginator = context['paginator']
-    page_numbers_range = 5
-    max_index = len(paginator.page_range)
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context['paginator']
+        page_numbers_range = 5
+        max_index = len(paginator.page_range)
 
-    page = self.request.GET.get('page')
-    current_page = int(page) if page else 1
-    start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
-    end_index = start_index + page_numbers_range
-    if end_index >= max_index:
-        end_index = max_index
+        page = self.request.GET.get('page')
+        current_page = int(page) if page else 1
+        start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+        end_index = start_index + page_numbers_range
+        if end_index >= max_index:
+            end_index = max_index
 
-    page_range = paginator.page_range[start_index:end_index]
-    context['page_range'] = page_range
+        page_range = paginator.page_range[start_index:end_index]
+        context['page_range'] = page_range
 
-    return context
+        return context
 
 @login_message_required
 def menu_detail_view(request,pk):
